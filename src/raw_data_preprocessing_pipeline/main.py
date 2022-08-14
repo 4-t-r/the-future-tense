a5127965a12c2a6fce1ef952a80c655ce0c86f32
Julia310 <JPeters92@protonmail.com>
2022-08-14 20:08:26 +0200

    classify labed test set with sentiment model

--- src/raw_data_preprocessing_pipeline/main.py
+++ src/raw_data_preprocessing_pipeline/main.py
@@ -1,15 +1,31 @@
 from satements import list_sq, list_neg
 from add_sentiment import SentimentClassifier
 import csv
+import os
+
+test_set_path = os.path.abspath("../../datasets/sentiment_dataset/sentiment_dataset.csv")
+sentiment_dict = {
+    'NEG': 0,
+    'NEU': 1,
+    'POS': 2
+}
+
+
+
+
+def get_sentiment_class_mapping(sentiments):
+    sentiment_class = []
+    for sentiment in sentiments:
+        sentiment_class.append(sentiment_dict[sentiment.output])
+
 
 def main():
+    statements = list(csv.reader(open(test_set_path, encoding="utf8")))
     sentiment_classifier = SentimentClassifier()
-    sentiments = sentiment_classifier.add_sentiment_three_labels(list_neg
-        )
-    #sentiment = sentiments[0].output
-    for i in range(len(sentiments)):
-        print(f'{list_neg[i]}| {sentiments[i]}')
-        #print(f'{sentiments[i]}')
+    sentiments = sentiment_classifier.add_sentiment_three_labels(statements)
+
+
+
 
 
 if __name__ == "__main__":
