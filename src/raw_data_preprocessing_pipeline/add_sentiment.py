f93526f26a7f4e51ff602bd55a442d76c8854111
Julia310 <JPeters92@protonmail.com>
2022-08-14 22:24:46 +0200

    model manual label delta

--- src/raw_data_preprocessing_pipeline/add_sentiment.py
+++ src/raw_data_preprocessing_pipeline/add_sentiment.py
@@ -7,20 +7,20 @@ from pysentimiento import SentimentAnalyzer
 
 class SentimentClassifier:
     def __init__(self):
-        self.classification_pipeline = pipeline('text-classification',
+        '''self.classification_pipeline = pipeline('text-classification',
                                                 model='distilbert-base-uncased-finetuned-sst-2-english',
-                                                device=-1)
+                                                device=-1)'''
         #self.sentiment_analyzer = SentimentAnalyzer(lang = 'en')
         self.model_path = 'cardiffnlp/twitter-roberta-base-sentiment-latest'
         self.sentiment_pipline = pipeline("sentiment-analysis", model=self.model_path, tokenizer=self.model_path)
         self.sentiment_analyzer = SentimentAnalyzer(lang='en')
 #sentiment_task("Covid cases are increasing fast!")
 
-    def add_sentiment_two_labels(self, statement_list):
+    '''def add_sentiment_two_labels(self, statement_list):
         sentiments_list = []
         for i in range(len(statement_list)):
             sentiments_list.append(self.classification_pipeline(statement_list[i])[0])
-        return sentiments_list
+        return sentiments_list'''
 
     def add_sentiment_three_labels(self, statement_list):
         #return self.sentiment_pipline(statement_list)
--- src/raw_data_preprocessing_pipeline/main.py
+++ src/raw_data_preprocessing_pipeline/main.py
@@ -2,6 +2,8 @@
 from add_sentiment import SentimentClassifier
 import csv
 import os
+from sklearn.metrics import confusion_matrix as cm
+import numpy as np
 
 test_set_path = os.path.abspath("../../datasets/sentiment_dataset/sentiment_dataset.csv")
 test_set_label_path = os.path.abspath("../../datasets/sentiment_dataset/labels.csv")
@@ -13,30 +15,79 @@ sentiment_dict = {
 }
 
 
-def get_sentiment_classification(statements):
-    sentiment_classifier = SentimentClassifier()
+def get_sentiment_classification(statements, sentiment_classifier):
     sentiments = sentiment_classifier.add_sentiment_three_labels(statements)
+    return sentiments
 
 
 def get_sentiment_class_mapping(sentiments):
     sentiment_class = []
     for sentiment in sentiments:
         sentiment_class.append(sentiment_dict[sentiment.output])
+    return sentiment_class
 
 
-def split_statements_in_chunks(statements, chunksize = 30):
+def label_chunks(statement_chunks, sentiment_classifier):
+    y_pred = []
+    for chunk in statement_chunks:
+        chunk_sentiments = get_sentiment_classification(chunk, sentiment_classifier)
+        chunk_sentiments_class = get_sentiment_class_mapping(chunk_sentiments)
+        y_pred = y_pred + chunk_sentiments_class
+        print(y_pred)
+    return y_pred
+
+
+
+
+def split_statements_in_chunks(statements, chunksize = 32):
     return [statements[i:i + chunksize] for i in range(0, len(statements), chunksize)]
 
 
 def get_labeled_test_data():
     statements = list(csv.reader(open(test_set_path, encoding="utf8")))
+    statements = [x[0] for x in statements]
     labels = list(csv.reader(open(test_set_label_path, encoding="utf8" )))
+    labels = [int(x[0]) for x in labels]
     return statements, labels
 
 
+def get_confusion_matrix(y_true, y_pred):
+    #y_true = list(map(int, y_true))
+    print(cm(np.asarray(y_true), np.asarray(y_pred)))
+
+
+def save_wrong_classified(y_true, y_pred, statements):
+    negative_by_model = open("negative_by_model.csv", 'w+', newline='')
+    neutral_by_model = open("neutral_by_model.csv", 'w+', newline='')
+    positive_by_model = open("positive_by_model.csv", 'w+', newline='')
+
+    csv_writer_neg = csv.writer(negative_by_model, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    csv_writer_neu = csv.writer(neutral_by_model, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    csv_writer_pos = csv.writer(positive_by_model, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+
+    for i in range(len(y_true)):
+        if y_true[i] != y_pred[i]:
+            if y_pred[i] == 0:
+                csv_writer_neg.writerow([statements[i], y_true[i]])
+            elif y_pred[i] == 1:
+                csv_writer_neu.writerow([statements[i], y_true[i]])
+            elif y_pred[i] == 2:
+                csv_writer_pos.writerow([statements[i], y_true[i]])
+
+
+
+
 def main():
+    sentiment_classifier = SentimentClassifier()
     statements, y_true = get_labeled_test_data()
     statement_chunks = split_statements_in_chunks(statements)
+    y_pred = label_chunks(statement_chunks, sentiment_classifier)
+    get_confusion_matrix(y_true, y_pred)
+    save_wrong_classified(y_true, y_pred, statements)
 
 
 
